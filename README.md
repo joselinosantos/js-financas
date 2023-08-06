98177603343cbb6986d1148942dd41eac9d1f3c8
Joselino Santos <joselinosantosti@gmail.com>
2023-08-06 17:25:03 -0300

    Implementação classe Despesa e função para salvar

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Sisteminha para gerenciamento de receitas e despesas
 
 3.2 Criar função e capturar valores do formulário
 
+3.3 Criar classe Despesa
+
+3.4 Instanciar a classe Despesa na função cadastrarDespesa
+
+4 Persistência no Local Storage
+
+4.1 Criar função para gravar, recebe parâmetro despesa.
+
+4.2 Converte o objeto recebido para JSON e executa a gravação.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+class Despesa {
+    constructor(ano, mes, dia, categoria, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.categoria = categoria
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -5,4 +16,19 @@ function cadastrarDespesa() {
     let categoria = document.getElementById('categoria')
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
+
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        categoria.value,
+        descricao.value,
+        valor.value
+    )
+    
+    gravar(despesa)
+}
+
+function gravar(despesa) {
+    localStorage.setItem('despesa', JSON.stringify(despesa))
 }
