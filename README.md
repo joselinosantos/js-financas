4b0c6d0efc87ccdfc97fb080054963a0bc827908
Joselino Santos <joselinosantosti@gmail.com>
2023-08-06 16:58:01 -0300

    Evento de click e captura de valores

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# js-financas
+# JS Finanças Pessoais
 Sisteminha para gerenciamento de receitas e despesas
+
+## Projeto
+1 Criação do projeto
+
+2 Preparação dos layouts para cadastro e listagem
+
+3 Cadastrar nova despesa
+
+3.1 Evento de click no link Cadastro
+
+3.2 Criar função e capturar valores do formulário
+
---
+++ js/app.js
@@ -0,0 +1,8 @@
+function cadastrarDespesa() {
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let categoria = document.getElementById('categoria')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+}
