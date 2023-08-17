58848c19a8e0037f27a25096eab0905981629884
Joselino Santos <joselinosantosti@gmail.com>
2023-08-17 14:43:02 -0300

    Removendo despesa, refatorado o modal

--- js/app.js
+++ js/app.js
@@ -66,34 +66,38 @@ class Db {
     }
 
     pesquisar(despesa) {
-		let despesasFiltradas = []
-		despesasFiltradas = this.listar()
+        let despesasFiltradas = []
+        despesasFiltradas = this.listar()
 
-		if (despesa.ano != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
-		}
+        if (despesa.ano != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
 
-		if (despesa.mes != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
-		}
+        if (despesa.mes != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
 
-		if (despesa.dia != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
-		}
+        if (despesa.dia != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
 
-		if (despesa.categoria != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.categoria == despesa.categoria)
-		}
+        if (despesa.categoria != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.categoria == despesa.categoria)
+        }
 
-		if (despesa.descricao != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
-		}
+        if (despesa.descricao != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
 
-		if (despesa.valor != '') {
-			despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
-		}
-		return despesasFiltradas
-	}
+        if (despesa.valor != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+        return despesasFiltradas
+    }
+
+    remover(id) {
+        localStorage.removeItem(id)
+    }
 }
 
 let bd = new Db()
@@ -113,32 +117,31 @@ function cadastrarDespesa() {
     if (despesa.validarDados()) {
         bd.salvar(despesa)
 
-        modalMessage()
+        modalMessage('Sucesso na gravação!', 'Despesa cadastrada com sucesso!')
         limparCampos()
     } else {
-        modalMessage('erro')
+        modalMessage('Erro na gravação!', 'Existem campos obrigatórios vazios!', 'erro')
     }
 
-    $('#modal-cadastro').modal('show')
+    $('#modal').modal('show')
 }
 
-function modalMessage(tipo) {
+function modalMessage(title, description, tipo) {
     let titulo = document.querySelector('.modal-titulo')
     let texto = document.querySelector('.modal-body')
     let btn = document.querySelector('.btn-modal')
     let classesBtn = 'btn btn-modal'
 
+    titulo.innerHTML = title
+    texto.innerHTML = description
+
     if (tipo === 'erro') {
         titulo.className = 'modal-titulo text-danger'
-        titulo.innerHTML = 'Erro na gravação!'
-        texto.innerHTML = 'Existem campos obrigatórios vazios!'
 
         btn.className = classesBtn + ' btn-danger'
         btn.innerHTML = 'Voltar e corrigir'
     } else {
         titulo.className = 'modal-titulo text-success'
-        titulo.innerHTML = 'Sucesso na gravação!'
-        texto.innerHTML = 'Despesa cadastrada com sucesso!'
 
         btn.className = classesBtn + ' btn-success'
         btn.innerHTML = 'OK'
@@ -165,7 +168,7 @@ function limparCampos() {
     valor.value = ''
 }
 
-function listarDespesas(despesas = [], filtro=false) {
+function listarDespesas(despesas = [], filtro = false) {
     if (despesas.length == 0 && filtro == false) {
         despesas = bd.listar()
     }
@@ -175,7 +178,7 @@ function listarDespesas(despesas = [], filtro=false) {
 
     despesas.forEach((d) => {
         let linha = listaDespesas.insertRow()
-
+        let btnExcluir = document.createElement('button')
         let categorias = {
             '1': 'Alimentação',
             '2': 'Educação',
@@ -186,18 +189,36 @@ function listarDespesas(despesas = [], filtro=false) {
 
         d.categoria = categorias[d.categoria]
 
+        btnExcluir.className = "btn btn-danger"
+        btnExcluir.innerHTML = "<i class='fa fa-trash'></i>"
+        btnExcluir.id = d.id
+
+        btnExcluir.onclick = () => {
+            modalMessage('Atenção!', 'Tem certeza que deseja deletar o registro ' + d.id + '?', 'erro')
+            $('#modal').modal('show')
+
+            let btnModal = document.querySelector('.btn-modal')
+            btnModal.innerHTML = "Sim"
+
+            btnModal.onclick = () => {
+                bd.remover('despesa' + d.id)
+                window.location.reload()
+            }
+        }
+
         linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
         linha.insertCell(1).innerHTML = d.categoria
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = `R$ ${d.valor}`
+        linha.insertCell(4).append(btnExcluir)
     })
 }
 
 function pesquisarDespesas() {
-	getCampos()
+    getCampos()
 
-	let despesa = new Despesa(ano.value, mes.value, dia.value, categoria.value, descricao.value, valor.value)
-	let despesas = 	bd.pesquisar(despesa)
+    let despesa = new Despesa(ano.value, mes.value, dia.value, categoria.value, descricao.value, valor.value)
+    let despesas = bd.pesquisar(despesa)
 
-	listarDespesas(despesas, true)
+    listarDespesas(despesas, true)
 }
